7ca5ce2ebc061a8ae05f17317fe8ec9ffaa036fc
Ralf Handl <ralf.handl@sap.com>
2024-12-04 14:29:54 +0100

    return code 1 if schema test coverage below 100%

--- scripts/schema-test-coverage.sh
+++ scripts/schema-test-coverage.sh
@@ -8,11 +8,15 @@ echo
 echo "Schema Test Coverage"
 echo
 
+rc=0
+
 for schemaDir in schemas/v* ; do
   version=$(basename "$schemaDir")
   echo $version
 
-  node scripts/schema-test-coverage.mjs $schemaDir/schema.yaml tests/$version/pass
+  node scripts/schema-test-coverage.mjs $schemaDir/schema.yaml tests/$version/pass || rc=1
 
   echo
 done
+
+exit $rc
