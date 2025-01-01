b333c2dc565cd4a568cc943caea7ccde87b3ac83
Bellangelo <aggelosbellos7@gmail.com>
2025-01-01 21:35:21 +0200

    Change script to require a config parameter

--- scripts/format-markdown.sh
+++ scripts/format-markdown.sh
@@ -2,7 +2,28 @@
 
 SRCDIR="$(dirname "${BASH_SOURCE[0]}")" # check on Windows
 
-for filename in $*; do
+CONFIG=""
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --config)
+      CONFIG="$2"
+      shift # Remove --config from processing
+      shift # Remove the value from processing
+      ;;
+    *)
+      FILES+="$1 "
+      shift # Move to the next argument
+      ;;
+  esac
+done
+
+if [ -z "$CONFIG" ]; then
+  echo "Error: --config parameter is required."
+  exit 1
+fi
+
+for filename in $FILES; do
   # mostly to format code blocks with examples, unfortunately messes up bullet lists and tables
   npx prettier --write --single-quote $filename
 
@@ -11,5 +32,5 @@ for filename in $*; do
   sed -E -e "s/  +\|/ |/g" -e "s/\|  +/| /g" -e "s/-----+/----/g" $filename > $filename.tmp && mv $filename.tmp $filename
 
   # repair the bullet lists and various other markdown formatting issues
-  npx --yes markdownlint-cli --fix --config $SRCDIR/../.markdownlint.yaml $filename
+  npx --yes markdownlint-cli --fix --config "$CONFIG" $filename
 done
